e60f8961418e8adc75fb59735239c7a8847989d2
yeobi01 <yeobi_01@naver.com>
2023-10-29 16:13:14 +0900

    [FEAT] PWA 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+};
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+        },
+      },
+    ],
+    // 추가 플러그인 작성
+  ],
+  nextConfig
+);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import SEO from "@/app/seo";
+import PWA from "@/app/pwa";
 
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SEO />
+      <PWA />
       <body className="flex flex-col">
         <Header />
         {children}
